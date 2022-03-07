dead06634831230547c6a6cf73b429d0dfa252cd
themode <themode@outlook.fr>
2022-03-07 12:11:38 +0100

    Update minestom

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
-    implementation("com.github.Minestom:Minestom:f7d44c4774")
+    implementation("com.github.Minestom:Minestom:94406d5380")
     implementation("io.github.spair:imgui-java-app:1.86.2")
 }
 
